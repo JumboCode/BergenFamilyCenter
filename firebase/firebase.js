64b1c1443eb1b5cdb0fdd28bba1c660019565a4e
Adam Peters <petersadam18@gmail.com>
2021-10-02 02:41:50 -0400

    Updated file structure

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-console.log(app);
-export default app;
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
