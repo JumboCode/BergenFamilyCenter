7f5b0526ff3c6114b1b1f81bc18b7e2aa07b1a26
adamgpeters <55671033+adamgpeters@users.noreply.github.com>
2021-10-22 15:41:51 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import { Typography, Button } from "@mui/material";
 import userSignUp from "../src/firebaseSignUp";
-import userSignIn from "../src/firebaseSignIn";
-import userSignOut from "../src/firebaseSignOut";
 
 
 export default function Home() {
