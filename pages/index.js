9dd4332015a6fd5bdb1d11ea57a31689b0b4e344
Eli Intriligator <elewint@gmail.com>
2021-11-12 16:24:27 -0500

    Added events
    Co-authored-by: emma-mp <emma-mp@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
 import { Typography, Button } from "@mui/material";
-import userSignUp from "../src/firebaseSignUp";
-import passwordReset from "../src/firebasePasswordReset";
 
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>Bergen Family Center</title>
-            </Head>
-
-            <Typography>bergenfamilycenter</Typography>
-        </div >
-    );
+  return (
+    <div>
+      <Head>
+        <title>Bergen Family Center</title>
+      </Head>
+      <Typography>bergenfamilycenter</Typography>
+    </div>
+  );
 }
---
+++ src/firebaseEvents.js
@@ -0,0 +1,48 @@
+import { collection, doc, setDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore"; 
+import { db } from "../firebase/firebase.js";
+
+const firebaseNewEvent = async (data) => {
+    // Add a new document with a generated id
+    const newEventRef = doc(collection(db, "events"));
+
+    await setDoc(newEventRef, data);
+    console.log("newEventRef: ", newEventRef);
+};
+
+const firebaseUpdateEvent = async (data, id) => {
+    // Find document with user-provided id
+    const eventRef = doc(db, "events", id);
+    
+    // Update the document data
+    await updateDoc(eventRef, {
+        attendees: data.attendees,
+        description: data.description,
+        manager: data.manager,
+        name: data.name,
+        subgroup: data.subgroup,
+        startTime: data.startTime,
+        endTime: data.endTime
+    });
+
+    console.log("newEventRef: ", eventRef);
+};
+
+const firebaseAppendUser = async (user, id) => {
+    const eventRef = doc(db, "events", id);
+    const userRef = doc(db, "users", id);
+
+    await updateDoc(eventRef, {
+        attendees: arrayUnion(userRef)
+    });
+}
+
+const firebaseRemoveUser = async (user, id) => {
+    const eventRef = doc(db, "events", id);
+    const userRef = doc(db, "users", id);
+
+    await updateDoc(eventRef, {
+        attendees: arrayRemove(userRef)
+    });
+}
+
+export {firebaseNewEvent, firebaseUpdateEvent, firebaseAppendUser, firebaseRemoveUser};
