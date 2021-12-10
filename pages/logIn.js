2894e8539acf8aad50bc7fc1d1580ca9ba253922
Kabir Pamnani <kabirgpamnani@gmail.com>
2021-12-10 16:06:04 -0500

    card

--- pages/logIn.js
+++ pages/logIn.js
@@ -1,12 +1,46 @@
 import Head from 'next/head'
-import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import GoogleButton from 'react-google-button'
+
+
+const useStyles = makeStyles({
+    GoogleButton: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        margin: '0 500px',
+
+
+
+
+
+
+    },
+
+})
 
 export default function LogIn() {
+    const classes = useStyles();
     return (
-        <div>
-            <Typography>
-                bergenfamilycenter
-            </Typography>
+        <div class="google-button">
+            <GoogleButton className={classes.GoogleButton}></GoogleButton>
+
         </div>
+
     )
+
 }
+
+
+
+
+
+
+
+
+
+
+
