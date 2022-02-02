1a8ca661751196b175a15465171682b23feb8d7d
Eli Intriligator <elewint@gmail.com>
2022-02-01 20:29:51 -0500

    Implemented paginated events!
    Co-authored-by: Emma Paterson <emma-mp@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import { Typography, Button } from "@mui/material";
+import { firebaseFilterEventsPaginate } from "../src/firebaseEvents"
 
 export default function Home() {
+  const divisions = ["Child"];
+  
   return (
     <div>
       <Head>
         <title>Bergen Family Center</title>
       </Head>
       <Typography>bergenfamilycenter</Typography>
+      <Button onClick={async () => {await firebaseFilterEventsPaginate(divisions, 5, 0)}}>This is a button</Button>
     </div>
   );
 }
