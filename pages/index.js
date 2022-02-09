75acee7efd040a735b18ce09e94badff3179766b
Eli Intriligator <elewint@gmail.com>
2022-02-08 20:37:29 -0500

    Removed testing code.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
-import { Button } from "@mui/material";
-import { firebaseRetrieveAttendees } from "../src/firebaseEvents";
 
 export default function Home() {
-  const id = "eventex";
+  const divisions = ["Child"];
 
   return (
     <div>
       <Head>
         <title>Bergen Family Center</title>
       </Head>
-      <Button onClick={async () => {await firebaseRetrieveAttendees(id)}}>please please click me to retrieve event's attendees</Button>
     </div>
   );
 }
