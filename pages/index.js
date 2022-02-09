5008d6d90abe0aa0fbeb56094310148eb31f077b
Eli Intriligator <elewint@gmail.com>
2022-02-08 20:30:49 -0500

    Retrieve attendees works! Test code here.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from "next/head";
+import { Button } from "@mui/material";
+import { firebaseRetrieveAttendees } from "../src/firebaseEvents";
 
 export default function Home() {
-  const divisions = ["Child"];
+  const id = "eventex";
 
   return (
     <div>
       <Head>
         <title>Bergen Family Center</title>
       </Head>
+      <Button onClick={async () => {await firebaseRetrieveAttendees(id)}}>please please click me to retrieve event's attendees</Button>
     </div>
   );
 }
