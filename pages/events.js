3ab1f83bc5a883f6674e9021b1d70e34863bdaaa
Kabir Pamnani <kabirpamnani@Kabirs-MacBook-Air.local>
2022-03-06 19:40:14 -0500

    events push

--- pages/events.js
+++ pages/events.js
@@ -1,31 +1,57 @@
+import { ClassNames } from '@emotion/react';
 import { Grid } from '@mui/material';
 import Event from "../components/event";
 import NavBar from "../components/navBar.js";
+
+
+
 
 export default function Events() {
     return (
         <div>
             <NavBar />
             <div style={{ margin: 10 }}>
-                <Grid container spacing={4}>
-                    <Grid item xs={3}>
+            <Grid container spacing={2} columns={16}>
+                    <Grid item xs="auto">
+                        <Event sx={{ width: 500, height: 275 }}
+                        image="/TransparentTree.png" title="Event Name" description="Event info.">
+                        </Event>
+                    </Grid>
+                    <Grid item xs="auto">
                         <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
                         </Event>
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs="auto">
                         <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
                         </Event>
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs="auto">
                         <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
                         </Event>
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs="auto">
+                        <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
+                        </Event>
+                    </Grid>
+                    <Grid item xs="auto">
+                        <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
+                        </Event>
+                    </Grid>
+                    <Grid item xs="auto">
+                        <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
+                        </Event>
+                    </Grid>
+                    <Grid item xs="auto">
+                        <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
+                        </Event>
+                    </Grid>
+                    <Grid item xs="auto">
                         <Event image="/TransparentTree.png" title="Event Name" description="Event info.">
                         </Event>
                     </Grid>
                 </Grid>
-            </div>
         </div>
+        </div>
+        
     )
 }
