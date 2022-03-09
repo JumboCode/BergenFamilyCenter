b1ecd656d38ea622155bc9f8b32e4d841511abb5
Adam Peters <petersadam18@gmail.com>
2022-03-08 19:59:39 -0500

    Merged With kabir

--- components/event.js
+++ components/event.js
@@ -13,35 +13,37 @@ import Image from 'next/image';
 export default function MediaCard({ description, title, image, className, startTime, endTime }) {
     const [loading, setLoading] = React.useState(true);
     return (
-        <Card sx={{ width: 225, height: 150, borderRadius: "0%", padding: 0 }} className={className}>
-            <CardActionArea sx={{ height: 159 }}>
-                <CardMedia
-                    sx={{ height: "60%" }}
-                    alt="bergen-family"
-                >
-                    {loading ? <Skeleton animation="wave" variant="rectangular" width="100%" height="100%" /> : null}
-                    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-                        <Image
-                            onLoadingComplete={() => setLoading(false)}
-                            src={image}
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </div>
-                </CardMedia>
-                <CardContent>
-                    {loading ?
-                        <Skeleton animation="wave" variant="text" /> :
-                        (<Typography variant="h6" component="div">
-                            {title}
-                        </Typography>)
-                    }
-                </CardContent>
-            </CardActionArea>
+        <div style={{ padding: 10 }}>
+            <Card sx={{ width: "100%", borderRadius: "2%", padding: 0 }} className={className}>
+                <CardActionArea sx={{ height: 159 }}>
+                    <CardMedia
+                        sx={{ height: "60%" }}
+                        alt="bergen-family"
+                    >
+                        {loading ? <Skeleton animation="wave" variant="rectangular" width="100%" height="100%" /> : null}
+                        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                            <Image
+                                onLoadingComplete={() => setLoading(false)}
+                                src={image}
+                                layout="fill"
+                                objectFit="cover"
+                            />
+                        </div>
+                    </CardMedia>
+                    <CardContent>
+                        {loading ?
+                            <Skeleton animation="wave" variant="text" /> :
+                            (<Typography variant="h6" component="div">
+                                {title}
+                            </Typography>)
+                        }
+                    </CardContent>
+                </CardActionArea>
 
-            {/* <CardActions>
+                {/* <CardActions>
                 <Button size="small">Sign Up</Button>
             </CardActions> */}
-        </Card>
+            </Card>
+        </div>
     );
 }
