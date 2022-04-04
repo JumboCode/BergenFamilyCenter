3b4b59afe66b1b97c46b83cfa0a153eefdd9fba5
Eli Intriligator <elewint@gmail.com>
2022-04-03 20:06:29 -0400

    Implemented getUpcomingUserEvents

--- src/userFunctions.js
+++ src/userFunctions.js
@@ -71,31 +71,3 @@ export const getUpcomingUserEvents = async (uid) => {
 
   return upcomingEvents;
 };
-
-// OLD AND BAD!
-// export const getUpcomingUserEvents = async (uid) => {
-//   const userRef = doc(db, "users", uid);
-//   const events = collection(db, "events");
-
-//   let upcoming_events = [];
-
-//   let now = new Date();
-//   now = new Date(now.setHours(0, 0, 0, 0));
-//   const last_midnight_timestamp = Timestamp.fromDate(now);
-
-//   let q = query(
-//     events,
-//     where("attendees", "array-contains", uid),
-//     orderBy("startTime"),
-//     where("startTime", ">=", last_midnight_timestamp)
-//   );
-
-//   const querySnapshot = await getDocs(q);
-
-//   querySnapshot.forEach((doc) => {
-//     upcoming_events.push(doc.data());
-//   });
-
-//   console.log(upcoming_events);
-//   return upcoming_events;
-// };
