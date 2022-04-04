3fdcb60cafafa506b5e0e7e4fd9f0d40cd8346b6
Archit <admin@MacBook-Air-63.local>
2022-04-03 20:03:39 -0400

    pop over done

--- components/event.js
+++ components/event.js
@@ -9,13 +9,88 @@ import CardHeader from '@mui/material/CardHeader';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import { Popover } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle'; 
+
 
 export default function MediaCard({ description, title, image, className, startTime, endTime }) {
     const [loading, setLoading] = React.useState(true);
+    const [numFields, setNumFields] = React.useState(1);
+
+    const formFields = [];
+    for (let z = 0; z < numFields; z ++) {
+        formFields.push(
+        <div>
+            <TextField
+                sx={{ my: 1, mx: 2, width: '20ch' }}
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Name"
+                variant="standard"
+            />
+            <TextField
+                sx={{ my: 1, mx: 2, width: '20ch' }}
+                autoFocus
+                margin="dense"
+                id="age"
+                label="Age"
+                variant="standard"
+            />
+        </div>
+        )
+    }
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        if (!open) {
+            setOpen(true);
+        }
+    };
+  
+    const handleClose = () => {
+        setOpen(false);
+    };
+  
     return (
         <div style={{ padding: 10 }}>
             <Card sx={{ width: "100%", borderRadius: "2%", padding: 0 }} className={className}>
-                <CardActionArea sx={{ height: 159 }}>
+                <CardActionArea 
+                    sx={{ height: 159 }}
+                    onClick={handleClickOpen}
+                    disableRipple={open}
+                >
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Enter Information</DialogTitle>
+                        <DialogContent>
+                            {formFields}
+                            </DialogContent>
+                        <DialogActions>
+                        <Button 
+                            onClick={() => {
+                                if (numFields < 10) {
+                                    setNumFields(numFields + 1)
+                            }}}>
+                            +
+                        </Button>
+                        <Button 
+                            onClick={() => {
+                                if (numFields > 1) {
+                                    setNumFields(numFields - 1)
+                            }}}>
+                            -
+                        </Button>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Submit</Button>
+                        </DialogActions>
+                    </Dialog>
+
                     <CardMedia
                         sx={{ height: "60%" }}
                         alt="bergen-family"
