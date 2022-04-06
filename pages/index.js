d17c0230cb153b92cd4ddb814311c56489fec4f3
Eli Intriligator <elewint@gmail.com>
2022-04-05 20:51:23 -0400

    Removed test code from main

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import * as React from "react";
-import MyUpcomingEvents from "../components/myUpcomingEvents";
-import Grid from "@mui/material/Grid"
 
 export default function Home() {
   return (
@@ -9,13 +7,6 @@ export default function Home() {
       <Head>
         <title>Bergen Family Center</title>
       </Head>
-      <Grid container>
-        <Grid item xs={3}>
-          <MyUpcomingEvents userID="Ap9SYtZOGdIFYKCJSs7e"></MyUpcomingEvents>
-        </Grid>
-        <Grid item xs={9}>
-        </Grid>
-      </Grid>
     </div >
   );
 }
