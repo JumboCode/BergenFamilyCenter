b863b9a8e8d08c14f6fd520789ac735c431a082c
emma-mp <emmapaterson135@gmail.com>
2022-04-19 19:49:49 -0400

    added manager event information to profile page

--- pages/profile.js
+++ pages/profile.js
@@ -14,6 +14,20 @@ import { updateUser } from "../src/userFunctions";
 import { firebaseUserPreviousEvents } from "../src/firebaseEvents";
 import { doc, getDoc, Timestamp } from "firebase/firestore";
 import UpcomingEvent from "../components/upcomingEvent.js";
+import Alert from '@mui/material/Alert';
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import EmailIcon from '@mui/icons-material/Email';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+
 
 
 const validationSchema = yup.object({
@@ -32,6 +46,8 @@ export default function Profile() {
     const auth = getAuth();
     const uid = auth.currentUser?.uid;
 
+    const [isManager, setIsManager] = useState(false)
+
     const [name, setName] = useState(auth.currentUser?.displayName ?? "you must be logged in");
     const [email, setEmail] = useState(auth.currentUser?.email ?? "you must be logged in");
     const [phoneNumber, setPhoneNumber] = useState("you must be logged in");
@@ -41,6 +57,9 @@ export default function Profile() {
     const [editing, setEditing] = useState(false);
 
     const [previousEvents, setPreviousEvents] = useState([]);
+    const [managerEvents, setManagerEvents] = useState([]);
+
+    const [copiedAlertVisible, setCopiedAlertVisible] = useState(false);
 
     useEffect(() => {
         if (uid) {
@@ -48,11 +67,36 @@ export default function Profile() {
             const currentTime = Timestamp.now()
 
             const userInfo = getDoc(userRef).then(value => {
+                setIsManager(value.data().isManager);
                 setName(value.data().name);
                 setEmail(value.data().email);
                 setPhoneNumber(value.data().phoneNumber);
                 setAddress(value.data().address);
                 firebaseUserPreviousEvents(currentTime).then(value => setPreviousEvents(value));
+
+                if (value.data().isManager) {
+
+                    const userRef = doc(db, "users", uid);
+
+                    getDoc(userRef).then(value => {
+                        const manager_events_run = value.data().eventsRun;
+                        const all_events = []
+                        manager_events_run.map((event) => {
+                            const all_attendees = []
+                            const all_emails = []
+                            getDoc(event.attendeesRef).then(attendeesDoc => {
+                                attendeesDoc.data().attendees.map(attendees => {
+                                    all_attendees.push(attendees.attendees);
+                                    getDoc(attendees.parent).then(parent => {
+                                        all_emails.push(parent.data().email)
+                                    })
+                                })
+                            });
+                            all_events.push({ name: event.name, eventAttendees: all_attendees, email: all_emails })
+                        });
+                        setManagerEvents(all_events)
+                    })
+                }
             });
         }
     }, []);
@@ -73,6 +117,75 @@ export default function Profile() {
             updateUser(uid, { address: values.address })
         },
     });
+
+
+
+    function NestedList() {
+        const [open, setOpen] = useState({});
+        const [emails, setEmails] = useState({});
+        useEffect(() => {
+            const listOpen = {}
+            const allEmails = {}
+            managerEvents.map((event) => {
+                listOpen[event.name] = false
+                allEmails[event.name] = event.email
+            })
+            setOpen(listOpen)
+            setEmails(allEmails)
+
+        }, []);
+
+        const handleClickList = (event) => {
+            const tempListOpen = { ...open }
+            tempListOpen[event.name] = !tempListOpen[event.name]
+            setOpen(tempListOpen);
+        };
+
+        const getEmails = (event) => {
+            navigator.clipboard.writeText(emails[event.name]);
+            setCopiedAlertVisible(true);
+        };
+
+        return (
+            <List
+                sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                    <ListSubheader component="div" id="nested-list-subheader">
+                        Event Being Managed:
+                    </ListSubheader>
+                }
+            >
+                {managerEvents.map((event) => {
+                    return <List key={event} >
+                        <ListItem>
+                            <ListItemButton onClick={async () => { await handleClickList(event) }}>
+                                <ListItemText primary={event.name} />
+                                {open[event.name] ? <ExpandLess /> : <ExpandMore />}
+                            </ListItemButton>
+                            <IconButton edge="end" aria-label="email" onClick={async () => { await getEmails(event) }}>
+                                <EmailIcon />
+                            </IconButton>
+                        </ListItem>
+                        <Collapse in={open[event.name]} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                {event.eventAttendees.map((attendees) => {
+                                    return Object.entries(attendees).map((person) => {
+                                        return <ListItemButton key={person} sx={{ pl: 4 }}>
+                                            <ListItemText primary={person[0] + ", " + person[1]} />
+                                        </ListItemButton>
+                                    })
+                                })
+                                }
+                            </List>
+                        </Collapse>
+                    </List>
+                })
+                }
+            </List>
+        );
+    }
 
     return (
         <div>
@@ -172,6 +285,29 @@ export default function Profile() {
                         null
                     }
                 </Box>
+                {isManager ?
+                    <Box style={{ width: "30%", margin: "2%" }}>
+                        < Typography component="h1" variant="h6" >
+                            {"Manager Functions:"}
+                        </Typography>
+
+                        <NestedList></NestedList>
+                        {copiedAlertVisible ?
+                            <Alert severity="success" action={
+                                <IconButton
+                                    aria-label="close"
+                                    color="inherit"
+                                    size="small"
+                                    onClick={() => {
+                                        setCopiedAlertVisible(false);
+                                    }}
+                                >
+                                    <CloseIcon fontSize="inherit" />
+                                </IconButton>
+                            }>Copied Emails!</Alert>
+                            : null}
+                    </Box>
+                    : null}
             </Box >
         </div >
     )
