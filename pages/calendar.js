fca6c33e5d091d9afa9c284d7da5ffc055db5cc5
daryaclark <darya.clark@tufts.edu>
2022-04-19 20:33:51 -0400

    Calendar filter for calendar page

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,10 +1,17 @@
+import * as React from "react";
 import { Typography, Grid } from '@mui/material';
 import NavBar from "../components/navBar.js";
 import { MonthCalendar } from "../components/calendar";
 import WeeklyCalendar from "../components/weeklyCalendar";
 import UpcomingEvent from "../components/upcomingEvent";
+import CheckboxLabels from '../components/calendarFilter.js';
+import { useState } from 'react';
 
 export default function Calendar() {
+    const [divisions, setDivisions] = useState([]);
+
+    // pull infor, set division to info when it arrives
+
     return (
         <div>
             <NavBar page={"calendar"}></NavBar>
@@ -19,7 +26,10 @@ export default function Calendar() {
                     <MonthCalendar />
                 </Grid>
                 <Grid item xs={7}>
-                    <WeeklyCalendar />
+                    <WeeklyCalendar divisions={divisions}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <CheckboxLabels divisions={divisions} setDivisions={setDivisions}/>
                 </Grid>
             </Grid>
         </div>
